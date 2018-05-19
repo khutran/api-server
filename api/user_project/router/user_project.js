17c7473ccee2346f6dc81dca529dbd11cbae384b
khoibn <buinguyenkhoi96@gmail.com>
2018-05-19 11:34:21 +0700

    tranformer

--- api/user_project/router/user_project.js
+++ api/user_project/router/user_project.js
@@ -1,45 +1,47 @@
-import * as _ from 'lodash';
 import express from 'express';
-import bcrypt from 'bcrypt-nodejs';
 import { Exception } from '../../../app/Exceptions/Exception';
 import UserProjectRepository from '../../../app/Repositories/UserProjectRepository';
 import { asyncMiddleware } from '../../../midlewares/AsyncMiddleware';
-import CheckSessionMiddleware from '../../../midlewares/CheckSessionMiddleware';
 import ApiResponse from '../../../app/Responses/ApiResponse';
 import UserProjectTransformer from '../../../app/Transformers/UserProjectTransformer';
 import AuthMiddleware from '../../../midlewares/AuthMiddleware';
-import SingletonService from "../../../app/Services/SingletonService";
+import hasPermission from '../../../midlewares/PermissionMiddleware';
+import Permission from '../../../app/Configs/AvailablePermissions';
 
 let router = express.Router();
 
 router.all('*', AuthMiddleware);
 
-router.post('/project', asyncMiddleware(add));
+router.post('/project', hasPermission.bind(Permission.ADMIN_VIEW), asyncMiddleware(add));
 
-async function add(req, res){
-  let data = {
-    user_id: req.body.user_id,
-    project_id: req.body.project_id
+async function add(req, res) {
+  try {
+    let data = {
+      user_id: req.body.user_id,
+      project_id: req.body.project_id
+    };
+
+    let repository = new UserProjectRepository();
+
+    let item = await repository
+      .where('user_id', data.user_id)
+      .where('project_id', data.project_id)
+      .first();
+
+    if (item) {
+      throw new Error('project exits', 1000);
+    }
+
+    let result = await repository.create(data);
+
+    if (!result) {
+      throw new Error('add fasle', 1000);
+    }
+
+    res.json(ApiResponse.item(result, new UserProjectTransformer()));
+  } catch (e) {
+    throw new Exception(e.message, 1000);
   }
-
-  let repository = new UserProjectRepository();
-
-  let item = await repository
-    .where('user_id', data.user_id)
-    .where('project_id', data.project_id)
-    .first();
-
-  if(item){
-    throw new Error('project exits', 1000);
-  }
-
-  let result = await repository.create(data);
-
-  if(!result){
-    throw new Error('add fasle', 1000);
-  }
-
-  res.json(ApiResponse.item(result, new UserProjectTransformer()));
 }
 
 module.exports = router;
--- app/Transformers/HostTransformer.js
+++ app/Transformers/HostTransformer.js
@@ -1,18 +1,18 @@
-import Transformer from "./Transformer";
-import HostSshKeyTransformer from "./HostSshKeyTransformer";
+import Transformer from './Transformer';
+import HostSshKeyTransformer from './HostSshKeyTransformer';
 
 export default class HostTransformer extends Transformer {
-    transform(model) {
-        return {
-            id: model.id,
-            name: model.name,
-            ip: model.ip,
-            created_at: model.created_at,
-            updated_at: model.updated_at
-        };
-    }
+  transform(model) {
+    return {
+      id: model.id,
+      name: model.name,
+      ip: model.ip,
+      created_at: model.created_at,
+      updated_at: model.updated_at
+    };
+  }
 
-    includeHostSshkeys(model) {
-        return this.collection(model.host_sshkeys, new HostSshKeyTransformer());
-    }
+  includeHostSshkeys(model) {
+    return this.collection(model.host_sshkeys, new HostSshKeyTransformer());
+  }
 }
