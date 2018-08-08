2c94358318cd833132001ae5bb3cb4c6bb1d40b3
khutx <khutx@vicoders.com>
2018-08-08 10:22:34 +0700

    api download

--- routes/api/v1/remote.js
+++ routes/api/v1/remote.js
@@ -3,7 +3,7 @@ import { AsyncMiddleware } from '../../../app/Middlewares/AsyncMiddleware';
 import { AuthMiddleware } from '../../../app/Middlewares/AuthMiddleware';
 import ApiResponse from '../../../app/Responses/ApiResponse';
 import { App } from '../../../app/Services/App';
-import { Exception } from '../../../app/Exceptions/Exception';
+// import { Exception } from '../../../app/Exceptions/Exception';
 // import { Auth } from '../../..//app/Services/Facades/Auth';
 // import jwt from 'jsonwebtoken';
 import AxiosRemoteServerTransformer from '../../../app/Transformers/AxiosRemoteServerTransformer';
@@ -11,7 +11,11 @@ import { RemoteServer } from '../../../app/Services/RemoteServer';
 
 const router = express.Router();
 
+router.get('/:id/download/database', AsyncMiddleware(downloadDb));
+router.get('/:id/download/source', AsyncMiddleware(downloadCode));
+
 router.all('*', AuthMiddleware);
+
 router.get('/:id', AsyncMiddleware(get));
 router.post('/:id', AsyncMiddleware(clone));
 router.put('/:id/pull', AsyncMiddleware(pull));
@@ -26,176 +30,130 @@ router.get('/:id/info', AsyncMiddleware(info));
 router.delete('/:id', AsyncMiddleware(deleteProject));
 router.delete('/:id/db', AsyncMiddleware(deleteDb));
 router.put('/:id/db', AsyncMiddleware(updateDb));
+router.get('/:id/config', AsyncMiddleware(getConfig));
+
+async function downloadCode(req, res) {
+  let id = req.params.id;
+  await App.make(RemoteServer).downloadCode(id, res);
+}
+
+async function downloadDb(req, res) {
+  let id = req.params.id;
+  await App.make(RemoteServer).downloadDb(id, res);
+}
 
 async function get(req, res) {
-  try {
-    // let pass = { invalid: false };
-    // let user = Auth.user();
-    // if (await user.isRole('superadmin')) {
-    //   pass['invalid'] = true;
-    // }
-    // const token = jwt.sign(
-    //   {
-    //     data: pass
-    //   },
-    //   process.env.JWT_SECRET,
-    //   { expiresIn: 60 }
-    // );
+  // let pass = { invalid: false };
+  // let user = Auth.user();
+  // if (await user.isRole('superadmin')) {
+  //   pass['invalid'] = true;
+  // }
+  // const token = jwt.sign(
+  //   {
+  //     data: pass
+  //   },
+  //   process.env.JWT_SECRET,
+  //   { expiresIn: 60 }
+  // );
 
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).checkDomain(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).checkDomain(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function updateDb(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).updateDb(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).updateDb(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function deleteDb(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).deleteDb(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).deleteDb(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function pull(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).pull(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).pull(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function deleteProject(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).deleteProject(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).deleteProject(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function info(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).info(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).info(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function getConfig(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).getConfig(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).getConfig(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function clone(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).clone(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).clone(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function createDb(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).createDb(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).createDb(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function createConfig(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).createConfig(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).createConfig(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function updateConfig(req, res) {
-  try {
-    const data = req.body;
-    const id = req.params.id;
-    const result = await App.make(RemoteServer)
-      .addData(data)
-      .updateConfig(id);
+  const data = req.body;
+  const id = req.params.id;
+  const result = await App.make(RemoteServer)
+    .addData(data)
+    .updateConfig(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    console.log(e);
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function runManager(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).runManager(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).runManager(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function firtsBuild(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).firtsBuild(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).firtsBuild(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 async function replaceDb(req, res) {
-  try {
-    const id = req.params.id;
-    const result = await App.make(RemoteServer).replaceDb(id);
+  const id = req.params.id;
+  const result = await App.make(RemoteServer).replaceDb(id);
 
-    res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
-  } catch (e) {
-    throw new Exception(e.response.data.message, e.response.data.error_code);
-  }
+  res.json(ApiResponse.item(result, new AxiosRemoteServerTransformer()));
 }
 
 export default router;
